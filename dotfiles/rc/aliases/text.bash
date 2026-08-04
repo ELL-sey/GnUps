0b9f4e3c22e9fdba0f0fe60f65300ef1667ddd73
ek <azytem@gmail.com>
2026-08-04 14:06:51 +0700

    add clear_comments

--- dotfiles/rc/aliases/text.bash
+++ dotfiles/rc/aliases/text.bash
@@ -2,3 +2,6 @@ alias grep_ips='grep -E -o "([0-9]{1,3}\.){3}[0-9]{1,3}"'
 alias grep_domains='grep  -E -o "\b[a-zA-Z0-9][a-zA-Z0-9.-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}\b"'
 alias ch='choose'
 alias sp='sponge'
+
+
+alias clear_comments='grep -vE "^#|^;"'
